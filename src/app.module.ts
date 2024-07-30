e33e67ce7f422f2cd8372da12820bafd2e06b4b3
Dmitry Kazantsev <dk@roboticket.com>
2024-07-30 12:57:50 +0200

    feat: docker & .env proccesing & tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { SensorDataModule } from 'src/sensor-data/sensor-data.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://ortvest:3LSwOTBneuHggpov@ortvest-core.yxuddrs.mongodb.net/',
-    ),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+
     SensorDataModule,
   ],
   controllers: [AppController],
